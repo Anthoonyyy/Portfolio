c786768709b5a3f461c005e989bfa9e170eb564e
Anthoonyyy <Anthony212007@hotmail.com>
2024-03-26 22:17:13 +0100

    Création de la navbar en bootstrap

---
+++ view/accueilView.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Portfolio Arnone Anthony</title>
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg" id="navbar">
+        <div class="container">
+            <a class="navbar-brand " href="#" id="logo">
+                <img src="../img/logo.png" alt="Logo" width="55" height="55" class="d-inline-block align-text-center">
+                Arnone Anthony
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto" id="contenuNav">
+
+                    <a class="nav-link active" aria-current="page" href="./">Accueil</a>
+                    <a class="nav-link" href="#">Profil</a>
+                    <a class="nav-link" href="#">Compétences</a>
+                    <a class="nav-link" href="#">Portfolio</a>
+                    <a class="nav-link" href="#">Contact</a>
+
+                </div>
+            </div>
+        </div>
+    </nav>
+
+</body>
+
+
+
+</html>
