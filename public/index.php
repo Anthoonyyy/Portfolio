5700a9ccf0e245ab8c58f659e5601a23c34e6209
Anthoonyyy <Anthony212007@hotmail.com>
2024-03-26 20:25:02 +0100

    gestion de soumission du formulaire pour insérer dans la db

--- public/index.php
+++ public/index.php
@@ -8,3 +8,27 @@ try {
 } catch (Exception $e) {
     die($e->getMessage());
 }
+
+// Si le formulaire a été soumis
+
+if (isset($_POST['firstname'], $_POST['lastname'], $_POST['usermail'], $_POST['message'])) {
+
+    // on appelle la fonction d'insertion dans la DB (addMessage())
+    $insert = addMessage($db, $_POST['firstname'], $_POST['lastname'], $_POST['usermail'], $_POST['message']);
+
+    //Si on obtient une erreur
+    if ($insert === true) $message = "Insertion réussie";
+    else $message = $insert;
+
+
+
+    // si l'insertion a réussi
+    if ($insert === true) { //Contient la fonction d'insertion
+        // on redirige vers la page actuelle
+        header("Location: ./");
+        exit();
+    } else {
+        // sinon, on affiche un message d'erreur
+        $message = "Erreur lors de l'insertion";
+    }
+}
