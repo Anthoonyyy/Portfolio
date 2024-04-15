f70f54e76308a3f4fc2bbe7d78dbe62b28266279
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-15 18:39:34 +0200

    Ajout du footer dans dossier inc et appel avec include

--- view/accueilView.php
+++ view/accueilView.php
@@ -44,18 +44,9 @@
 
 
     <!-------------------------------------FOOTER------------------------------------------------->
-    <div class=" container-fluid">
-        <footer class="py-3 my-4">
-            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Accueil</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Profil</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Compétences</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Portfolio</a></li>
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Contact</a></li>
-            </ul>
-            <p class="text-center text-body-secondary">© Arnone Anthony</p>
-        </footer>
-    </div>
+    <?php
+    include("inc/footer.php");
+    ?>
     <!-------------------------------------FIN FOOTER------------------------------------------------->
     <script src="../js/script.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
---
+++ view/inc/footer.php
@@ -0,0 +1,12 @@
+<div class=" container-fluid">
+    <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Profil</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Compétences</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Portfolio</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Contact</a></li>
+        </ul>
+        <p class="text-center text-body-secondary">© Arnone Anthony</p>
+    </footer>
+</div>
