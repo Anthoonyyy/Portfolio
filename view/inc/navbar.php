37439f07c96f7c1a462f72164f67be07937ae2b6
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-15 18:35:02 +0200

    Ajout d'un dossier inc dans la vue

---
+++ view/inc/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg sticky-top" id="navbar">
+    <div class="container">
+        <a class="navbar-brand " href="./" id="logo"> Arnone Anthony<span class="orange">.</span> </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav  ms-auto" id="contenuNav">
+
+                <a class="nav-link active" aria-current="page" href="./">Accueil</a>
+                <a class="nav-link" href="#">Profil</a>
+                <a class="nav-link" href="#">Compétences</a>
+                <a class="nav-link" href="#">Portfolio</a>
+                <a class="nav-link" href="#">Contact</a>
+
+            </div>
+        </div>
+    </div>
+</nav>
