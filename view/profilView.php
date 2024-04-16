c24271d1b497311f002aa9415e7a8625a420d51a
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-16 14:50:05 +0200

    Ajout du lien css pour page profil

--- view/profilView.php
+++ view/profilView.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
     <title><?=$title?></title>
 </head>
-<body>
+<body class="homePage">
     <h1>Profil</h1>
 </body>
 </html>
