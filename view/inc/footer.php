ad682ba64b504b8b87bb93cc2b2523c85d322ed6
Anthoonyyy <Anthony212007@hotmail.com>
2024-04-17 18:24:25 +0200

    Changement du footer

--- view/inc/footer.php
+++ view/inc/footer.php
@@ -1,12 +1,8 @@
-<div class=" container-fluid">
-    <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Accueil</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Profil</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Compétences</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Portfolio</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Contact</a></li>
-        </ul>
-        <p class="text-center text-body-secondary">© Arnone Anthony</p>
-    </footer>
-</div>
+<footer class=" text-center text-lg-start" id="footer">
+    <!-- Copyright -->
+    <div class="text-center p-3 text-white ">
+        © 2024 - Arnone Anthony
+
+    </div>
+    <!-- Copyright -->
+</footer>
