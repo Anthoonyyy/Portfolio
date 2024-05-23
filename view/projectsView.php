743630dfb37f5fce24a34e707a957e4df7b56ef3
Anthoonyyy <Anthony212007@hotmail.com>
2024-05-23 17:54:09 +0200

    changement nom classe body

--- view/projectsView.php
+++ view/projectsView.php
@@ -14,7 +14,7 @@
     <title><?= $title ?></title>
 </head>
 
-<body class="homePage">
+<body class="projectsPage">
     <!-------------------------------------NAVBAR------------------------------------------------->
     <?php
     include("inc/navbar.php")
