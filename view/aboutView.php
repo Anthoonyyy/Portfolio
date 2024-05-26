4d5604be558eb675d7f8c1e5f9e6a58ecfa3febb
Anthoonyyy <Anthony212007@hotmail.com>
2024-05-26 14:10:57 +0200

    ajout redirection page contact

--- view/aboutView.php
+++ view/aboutView.php
@@ -24,20 +24,20 @@
 
 
     <div class="container px-4 py-5">
-        <h2 class="pb-2 mt-5 text-center aboutMe">About me</h2>
+        <h2 class="pb-2 mt-3 text-center aboutMe">About me</h2>
 
         <div class="row row-cols-1 row-cols-md-2   py-5">
             <div class="col gap-2">
-                <h2 id="moreAboutMe" class="pb-4 pt-4">Know More About Me</h2>
-                <p class="text-white">I am a full-stack developer with a year and a half of training at <a href="https://www.cf2m.be/formation/developpeur-web" target="_blank" id="cf2m" class="animate__animated animate__jello  animate__repeat-2">CF2M</a>. I have a particular affinity for front-end development, where I love to express my creativity. I invite you to check out the <a href="?section=projects" id="redirectProjects" class="animate__animated animate__jello animate__delay-3s animate__repeat-2 "><span id="spanProj">P</span>rojects</a> section to see some of my latest work.</p>
+                <h2 class="pb-4 pt-4 moreAboutMe">Know More About Me</h2>
+                <p class="textAboutMe">I am a full-stack developer with a year and a half of training at <a href="https://www.cf2m.be/formation/developpeur-web" target="_blank" id="cf2m" class="animate__animated animate__jello  animate__repeat-2">CF2M</a>. I have a particular affinity for front-end development, where I love to express my creativity. I invite you to check out the <a href="?section=projects" id="redirectProjects" class="animate__animated animate__jello animate__delay-3s animate__repeat-2 "><span id="spanProj">P</span>rojects</a> section to see some of my latest work.</p>
 
-                <p class="text-white">Throughout my training, I have acquired numerous skills that are essential in web development. I am very enthusiastic about the opportunity to evolve in this field, continue learning, tackle new challenges, and push my limits. I am eager to contribute my knowledge while growing both personally and professionally.</p>
+                <p class="textAboutMe">Throughout my training, I have acquired numerous skills that are essential in web development. I am very enthusiastic about the opportunity to evolve in this field, continue learning, tackle new challenges, and push my limits. I am eager to contribute my knowledge while growing both personally and professionally.</p>
 
-                <p class="text-white">I am open to all opportunities and available for new ventures. Please feel free to contact me if you have any questions or if you would like to discuss potential collaborations.</p>
+                <p class="textAboutMe">I am open to all opportunities and available for new ventures. Please feel free to <a href="?section=contact" id="redirectContact" class="animate__animated animate__jello animate__delay-5s animate__repeat-2 animate_slow"> Contact </a> me if you have any questions or if you would like to discuss potential collaborations.</p>
             </div>
 
             <div class="col text-center ps-5 pt-3">
-                <h2 class="text-white">Skills</h2>
+                <h2 class="moreAboutMe">Skills</h2>
                 <div class="skills d-flex justify-content-center flex-wrap mt-5">
                     <div class="skillsItem ">HTML</div>
                     <div class="skillsItem ">CSS</div>
