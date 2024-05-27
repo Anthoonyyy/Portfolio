900ff694a4098d6d714f70253246d44837bba3ab
Anthoonyyy <Anthony212007@hotmail.com>
2024-05-27 17:18:53 +0200

    mise en place des cartes projets, à finir

--- view/projectsView.php
+++ view/projectsView.php
@@ -21,7 +21,66 @@
     ?>
     <!-------------------------------------FIN NAVBAR------------------------------------------------->
 
-    <h1>Projects</h1>
+    <h1 class="pb-3 mt-3 text-center aboutMe ">Projects</h1>
+    <div class="card mb-3 mt-3 cardContainer">
+        <div class="row g-0 ">
+            <div class="col-md-4">
+                <img src="img/projets2.png" alt="Projects2" class="img-fluid rounded-start imgProjects">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-white">Projects 2</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mb-3 mt-3 cardContainer">
+        <div class="row g-0 ">
+            <div class="col-md-4">
+                <img src="img/projets3.png" alt="Projects 3" class="img-fluid rounded-start imgProjects">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-white">Projects 3</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mb-3 mt-3 cardContainer">
+        <div class="row g-0 ">
+            <div class="col-md-4">
+                <img src="img/projetsBronks.png" alt="Trendy Pants and Shoes" class="img-fluid rounded-start imgProjects">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-white">Projects 4</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
