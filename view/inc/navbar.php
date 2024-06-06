72e783b489ac7aca548e7f09298fc979e6d8d161
Anthoonyyy <Anthony212007@hotmail.com>
2024-06-06 14:07:29 +0200

    Ajout classe text-white pour rendre mes liens de nav blanc

--- view/inc/navbar.php
+++ view/inc/navbar.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg sticky-top" id="navbar">
     <div class="container">
-        <a class="navbar-brand " href="?section=home" id="logo"> Arnone Anthony. </a>
+        <a class="navbar-brand text-white" href="?section=home" id="logo"> Arnone Anthony. </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav  ms-auto" id="contenuNav">
 
-                <a class="nav-link" href="?section=home">Home</a>
-                <a class="nav-link" href="?section=about">About</a>
-                <a class="nav-link" href="?section=projects">Projects</a>
-                <a class="nav-link" href="?section=contact">Contact</a>
+                <a class="nav-link text-white" href="?section=home">Home</a>
+                <a class="nav-link text-white" href="?section=about">About</a>
+                <a class="nav-link text-white" href="?section=projects">Projects</a>
+                <a class="nav-link text-white" href="?section=contact">Contact</a>
 
             </div>
         </div>
