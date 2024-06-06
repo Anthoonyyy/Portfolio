a719249a7ff716d6ef784f005898198e4593c747
Anthoonyyy <Anthony212007@hotmail.com>
2024-06-06 14:28:06 +0200

    changement du nom du fichier en francais

---
+++ view/projetsView.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Aladin&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet">
+    <title><?= $title ?></title>
+</head>
+
+<body class="projectsPage">
+    <!-------------------------------------NAVBAR------------------------------------------------->
+    <?php
+    include("inc/navbar.php")
+    ?>
+    <!-------------------------------------FIN NAVBAR------------------------------------------------->
+
+    <h1 class="pb-3 mt-3 text-center aboutMe ">Projects</h1>
+    <div class="card mb-3 mt-3 cardContainer">
+        <div class="row g-0 ">
+            <div class="col-md-4">
+                <img src="img/proj1.png" alt="Project1" class="img-fluid rounded-start imgProjects">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-white">Project 1</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mb-3 mt-3 cardContainer">
+        <div class="row g-0 ">
+            <div class="col-md-4">
+                <img src="img/proj2.png" alt="Project2" class="img-fluid rounded-start imgProjects">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-white">Project 2</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mb-3 mt-3 cardContainer">
+        <div class="row g-0 ">
+            <div class="col-md-4">
+                <img src="img/proj3.png" alt="Project3" class="img-fluid rounded-start imgProjects">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-white">Project 3</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="card mb-3 mt-3 cardContainer">
+        <div class="row g-0 ">
+            <div class="col-md-4">
+                <img src="img/projBronks.png" alt="Project Bronks" class="img-fluid rounded-start imgProjects">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title text-white">Project 4</h5>
+                    <p class="card-text">
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+
+</html>
