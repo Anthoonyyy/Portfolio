a7d3b77c465124b86eb036930e8c4cd88966baad
Anthoonyyy <Anthony212007@hotmail.com>
2024-07-01 18:57:05 +0200

    changement lien navbar en noir

--- view/inc/navbar.php
+++ view/inc/navbar.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg sticky-top" id="navbar">
     <div class="container">
-        <a class="navbar-brand text-white" href="?section=accueil" id="logo"> Arnone Anthony. </a>
+        <a class="navbar-brand " href="?section=accueil" id="logo"> Arnone Anthony. </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav  ms-auto" id="contenuNav">
 
-                <a class="nav-link text-white me-3" href="?section=accueil">Accueil</a>
-                <a class="nav-link text-white me-3" href="?section=Apropos">A propos</a>
-                <a class="nav-link text-white me-3" href="?section=projets">Projets</a>
-                <a class="nav-link text-white ms-2" href="?section=contact">Contact</a>
+                <a class="nav-link text-black me-3" href="?section=accueil">Accueil</a>
+                <a class="nav-link text-black me-3" href="?section=Apropos">A propos</a>
+                <a class="nav-link text-black me-3" href="?section=projets">Projets</a>
+                <a class="nav-link text-black ms-2" href="?section=contact">Contact</a>
 
             </div>
         </div>
