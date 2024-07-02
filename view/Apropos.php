f99e51639fc679d5835c1c983d20764014032975
Anthoonyyy <Anthony212007@hotmail.com>
2024-07-02 17:59:46 +0200

    mise en place de la section a propos

--- view/Apropos.php
+++ view/Apropos.php
@@ -11,52 +11,34 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Aladin&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title><?= $title ?></title>
 </head>
 
-<body class="aboutPage">
+<body id="bodyApropos">
     <!-------------------------------------NAVBAR------------------------------------------------->
     <?php
     include("inc/navbar.php")
     ?>
     <!-------------------------------------FIN NAVBAR------------------------------------------------->
 
-
-
-    <div class="container px-4 py-5">
-        <h2 class="pb-2 mt-3 text-center aboutMe">About me</h2>
-
-        <div class="row row-cols-1 row-cols-md-2   py-5">
-            <div class="col gap-2">
-                <h2 class="pb-4 pt-4 moreAboutMe">Know More About Me</h2>
-                <p class="textAboutMe">I am a full-stack developer with a year and a half of training at <a href="https://www.cf2m.be/formation/developpeur-web" target="_blank" id="cf2m" class="animate__animated animate__jello  animate__repeat-2">CF2M</a>. I have a particular affinity for front-end development, where I love to express my creativity. I invite you to check out the <a href="?section=projets" id="redirectProjects" class="animate__animated animate__jello animate__delay-3s animate__repeat-2 "><span id="spanProj">P</span>rojects</a> section to see some of my latest work.</p>
-
-                <p class="textAboutMe">Throughout my training, I have acquired numerous skills that are essential in web development. I am very enthusiastic about the opportunity to evolve in this field, continue learning, tackle new challenges, and push my limits. I am eager to contribute my knowledge while growing both personally and professionally.</p>
-
-                <p class="textAboutMe">I am open to all opportunities and available for new ventures. Please feel free to <a href="?section=contact" id="redirectContact" class="animate__animated animate__jello animate__delay-5s animate__repeat-2 animate_slow"> Contact </a> me if you have any questions or if you would like to discuss potential collaborations.</p>
+    <div class="container  px-4 py-5">
+        <h2 class="pb-2 mt-3 text-center aboutMe">A propos de moi</h2>
+        <div class="row flex-lg-row-reverse align-items-center s py-5">
+            <div class="col-10 col-sm-8 col-lg-6">
+                <img src="img/conceptCoding.png" class="d-block ms-5 img-fluid" alt="concept coding" width="700" height="500" loading="lazy">
             </div>
+            <div class="col-lg-6">
+                <p class="aMonPropos">Bonjour !</p>
+                <p class="aMonPropos"> Je suis un développeur web junior formé au CF2M, où j'ai suivi une formation intensive d'un an et demi. Avant de me lancer dans le développement web, j'ai travaillé dans le secteur de la vente.</p>
+                <p class="aMonPropos"> Cette expérience m'a permis d'acquérir de solides compétences en communication et en gestion des relations clients, qui se révèlent très utiles dans mon parcours actuel de développeur web. </p>
+                <p class="aMonPropos">Le métier de développeur m'a toujours passionné, mais ce n'est que récemment que j'ai eu l'opportunité de m'y consacrer pleinement. </p>
+                <p class="aMonPropos">Je suis une personne naturellement curieuse, toujours en quête d'apprentissage et de perfectionnement. Je cherche une entreprise qui me fera confiance et me permettra d'évoluer dans ce domaine, afin de pouvoir y contribuer activement tout en élargissant mes connaissances et compétences.</p>
 
-            <div class="col text-center ps-5 pt-3">
-                <h2 class="moreAboutMe">Skills</h2>
-                <div class="skills d-flex justify-content-center flex-wrap mt-5">
-                    <div class="skillsItem ">HTML</div>
-                    <div class="skillsItem ">CSS</div>
-                    <div class="skillsItem ">Javascript</div>
-                    <div class="skillsItem ">PHP</div>
-                    <div class="skillsItem ">SQL</div>
-                    <div class="skillsItem ">Bootstrap</div>
-                    <div class="skillsItem ">Jquery</div>
-                    <div class="skillsItem ">GIT</div>
-                    <div class="skillsItem ">Github</div>
-                    <div class="skillsItem ">Wordpress</div>
-                    <div class="skillsItem ">Responsive Design</div>
-                </div>
             </div>
-
-
-
         </div>
     </div>
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
