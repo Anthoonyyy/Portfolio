f13b5ccd8675316e196c7b15b3b24f262eeeb8f1
Anthoonyyy <Anthony212007@hotmail.com>
2024-07-15 19:09:30 +0200

    commencement page contact

--- view/contactView.php
+++ view/contactView.php
@@ -14,13 +14,13 @@
     <title><?= $title ?></title>
 </head>
 
-<body class="contactPage">
+<body id="bodyContact">
     <!-------------------------------------NAVBAR------------------------------------------------->
     <?php
     include("inc/navbar.php")
     ?>
     <!-------------------------------------FIN NAVBAR------------------------------------------------->
-    <h1>Contact</h1>
+    <h1 class="pb-3 mt-4 text-center" id="titreContact">Contactez-moi</h1>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
