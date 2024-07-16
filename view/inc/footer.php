400c74c2090fb4a3e3e47788ae6badd5bd54da68
Anthoonyyy <Anthony212007@hotmail.com>
2024-07-16 19:47:55 +0200

    ajout d'un tiret pour separer

--- view/inc/footer.php
+++ view/inc/footer.php
@@ -1,6 +1,6 @@
 <div id="containerFooter">
     <footer class="py-3  border-top" id="footer">
-        <p class="text-center text-white mt-2 texteFooter">© 2024 Arnone Anthony.</p>
+        <p class="text-center text-white mt-2 texteFooter">© 2024 - Arnone Anthony.</p>
         <p class="text-center text-white texteFooter "> Tous droits réservés.</p>
     </footer>
 </div>
